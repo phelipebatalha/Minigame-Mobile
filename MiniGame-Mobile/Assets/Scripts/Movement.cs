b9d6f05740f701250fb350b0c1afe8eccd4b4497
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-04-27 22:58:33 -0300

    Revert "Merge branch 'MovementAndCamera'"
    This reverts commit d1474cbc0a2c8e293ce2c3d9aafd687956f5ebd0, reversing
    changes made to bcc70c22fcec302663209f6fb2e7e14ddf5f6ce1.

--- MiniGame-Mobile/Assets/Scripts/Movement.cs
+++ MiniGame-Mobile/Assets/Scripts/Movement.cs
@@ -2,26 +2,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    [SerializeField] private float _rotateSpeed;
+    public float _speed;
     
     void Update()
     {
         Vector3 dir = Vector3.zero;
-        Vector3 rot = Vector3.zero;
 
-        rot.y = Input.acceleration.x;
+        dir.x = Input.acceleration.x;
         dir.z = Input.acceleration.y;
 
         if(dir.sqrMagnitude > 1)
             dir.Normalize();
-        if (rot.sqrMagnitude > 1) 
-            rot.Normalize();
         
         dir *= Time.deltaTime;
-        rot *= Time.deltaTime;
 
         transform.Translate(dir * _speed);
-        transform.Rotate(rot * _rotateSpeed);
     }
 }
