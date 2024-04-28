92635123c3ab3843f04154ce6ed3e32246a3dbef
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-04-27 21:52:25 -0300

    Mecanica de gun e adicionado efeitos

--- MiniGame-Mobile/Assets/Scripts/Bullet.cs
+++ MiniGame-Mobile/Assets/Scripts/Bullet.cs
@@ -13,6 +13,7 @@ public class Bullet : MonoBehaviour
  
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Enemy")
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
