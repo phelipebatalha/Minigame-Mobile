f22cb6e9d02033872100089ec7af4a52d3be254e
dextrux <titolucas1@live.com>
2024-04-27 22:12:01 -0300

    Adição de rotação da câmera

--- MiniGame-Mobile/Assets/Scripts/Movement.cs
+++ MiniGame-Mobile/Assets/Scripts/Movement.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float _speed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotateSpeed;
     
     void Update()
     {
         Vector3 dir = Vector3.zero;
+        Vector3 rot = Vector3.zero;
 
-        dir.x = Input.acceleration.x;
+        rot.y = Input.acceleration.x;
         dir.z = Input.acceleration.y;
 
         if(dir.sqrMagnitude > 1)
             dir.Normalize();
+        if (rot.sqrMagnitude > 1) 
+            rot.Normalize();
         
         dir *= Time.deltaTime;
+        rot *= Time.deltaTime;
 
         transform.Translate(dir * _speed);
+        transform.Rotate(rot * _rotateSpeed);
     }
 }
