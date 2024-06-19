b4b2f9681d17af544a1ce10523ac35e28da15cd2
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-06-19 19:53:28 -0300

    Adicionei responsividade de configurações e Shader

--- MiniGame-Mobile/Assets/Scripts/Configuracoes.cs
+++ MiniGame-Mobile/Assets/Scripts/Configuracoes.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Configuracoes : MonoBehaviour
 {
     public Movement movement;
-    [SerializeField] private float maxSliderSpeed = 20f, maxSliderRot = 0.01f;
+    [SerializeField] private float maxSliderSpeed = 50f, maxSliderRot = 0.015f;
     void Awake()
     {
         movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
@@ -18,7 +18,7 @@ public class Configuracoes : MonoBehaviour
     public void Sensibilidade(float value)
     {
         if(movement != null)
-        movement._rotateSpeed = value * maxSliderRot;
+        movement.inclination = value * maxSliderRot;
     }
     public void Sound(float value)
     {
