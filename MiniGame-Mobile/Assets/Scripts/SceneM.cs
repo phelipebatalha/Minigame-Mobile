dc2aec606e90c0a37ce69e56d3189f87e8de1894
Phelipe Henrique Batalha <phelipebatalha@hotmail.com>
2024-06-23 18:15:28 -0300

    Created a rank

--- MiniGame-Mobile/Assets/Scripts/SceneM.cs
+++ MiniGame-Mobile/Assets/Scripts/SceneM.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class SceneM : MonoBehaviour
 {
     public string sceneName;
+    public GameObject rank;
     public void LoadScene()
     {
         SceneManager.LoadScene(sceneName);
+    }
+    public void Ranking()
+    {
+        rank.SetActive(!rank.activeInHierarchy);
     }
 }
